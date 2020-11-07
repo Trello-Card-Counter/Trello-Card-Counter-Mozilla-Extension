58acbdc849ed98e5e80387ed6fe814fee537ec25
Adam Jaamour <adam@jaamour.com>
2020-11-08 00:39:49 +0100

    Use a toggle switch rather than 2 buttons

--- popup/toggle_count.js
+++ popup/toggle_count.js
@@ -1,12 +1,13 @@
+browser.tabs.executeScript({file: "../content_scripts/show_card_counter.js"})
+
 document.addEventListener("click", (e) => {
 
-  if (e.target.id === "on") {
+  if (e.target.checked === true) {
     browser.tabs.executeScript({file: "../content_scripts/show_card_counter.js"})
   }
 
-  if (e.target.id === "off") {
+  if (e.target.checked === false) {
     browser.tabs.executeScript({file: "../content_scripts/hide_card_counter.js"})
   }
 
-  e.preventDefault();
 });
