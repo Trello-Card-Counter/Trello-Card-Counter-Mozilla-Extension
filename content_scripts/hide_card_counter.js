b613901a1b47ed69a56bbe208f50487e18959d09
Adam Jaamour <adam@jaamour.com>
2020-11-08 12:25:27 +0100

    Add comments

--- content_scripts/hide_card_counter.js
+++ content_scripts/hide_card_counter.js
@@ -1,3 +1,8 @@
+/**
+ * Add the 'hide' class to each 'js-num-cards' element for each column to hide the number of cards. This number is
+ * already calculated by Trello but is not being used, so this function simply adds back the 'hide' class to not
+ * display it.
+ */
 (function () {
   document.querySelectorAll('.js-num-cards').forEach(function (list) {
     list.classList.add('hide');
--- content_scripts/show_card_counter.js
+++ content_scripts/show_card_counter.js
@@ -1,3 +1,8 @@
+/**
+ * Removes the 'hide' class to each 'js-num-cards' element for each column to show the number of cards. This number is
+ * already calculated by Trello but is not being used, so this function simply removes the 'hide' class to display the
+ * number in each column.
+ */
 (function () {
   document.querySelectorAll('.js-num-cards').forEach(function (list) {
     list.classList.remove('hide');
