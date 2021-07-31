9f57fef5725de5c5c2c22145fc59e8cd64c204fb
Adam Jaamour <adam@jaamour.com>
2021-07-31 19:50:37 +0100

    Rename js scripts

--- popup/popup_listener.js
+++ popup/popup_listener.js
@@ -2,19 +2,19 @@
 browser.tabs.executeScript({file: "../content_scripts/create_total_cards_button.js"});
 
 // Automatically show card counter when the extension popup is opened (default is "On").
-browser.tabs.executeScript({file: "../content_scripts/show_card_counter.js"});
+browser.tabs.executeScript({file: "../content_scripts/show_card_counters.js"});
 
 // Listen for events on the on/off toggle switch button to decide which script to run.
 document.addEventListener("click", (e) => {
 
   // Show the  number of Trello card in each column.
   if (e.target.checked === true) {
-    browser.tabs.executeScript({file: "../content_scripts/show_card_counter.js"});
+    browser.tabs.executeScript({file: "../content_scripts/show_card_counters.js"});
   }
 
   // Hide the  number of Trello card in each column.
   if (e.target.checked === false) {
-    browser.tabs.executeScript({file: "../content_scripts/hide_card_counter.js"});
+    browser.tabs.executeScript({file: "../content_scripts/hide_card_counters.js"});
   }
 
 });
