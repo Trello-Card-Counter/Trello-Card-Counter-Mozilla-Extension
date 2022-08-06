3a946afa88700ba866521abdeac234a0efa5f49a
Adam Jaamour <dev@jaamour.com>
2022-08-06 20:09:16 +0200

    Refactor JS and update comments

--- content_scripts/create_total_cards_button.js
+++ content_scripts/create_total_cards_button.js
@@ -3,18 +3,23 @@
  * board.
  */
 (() => {
-  // Count number of cards.
-  const numberOfCards = document.querySelectorAll(".list-card").length;
-  // Check if button already exists before creating it.
-  const buttonInstance = document.getElementById("button-total-cards-count");
-  // Create HTML for button.
-  const numberOfCardsButtonHTML = `<a id="button-total-cards-count" class="board-header-btn board-header-btn-invite ' +
+    // Count number of cards.
+    const numberOfCards = document.querySelectorAll(".list-card").length;
+
+    // Create on/off button instance.
+    const buttonInstance = document.getElementById("button-total-cards-count");
+
+    // Create HTML for button.
+    const numberOfCardsButtonHTML = `<a id="button-total-cards-count" class="board-header-btn board-header-btn-invite ' +
     'board-header-btn-without-icon" title="Invite to board"><span class="board-header-btn-text">' +
     'Total cards: <b>' ${numberOfCards} '</b></span></a>`;
-  // inner HTML of header buttons
-  let boardHeaderButtons = document.getElementsByClassName(
-    "board-header-btns mod-left"
-  )[1].innerHTML;
 
-  if (!buttonInstance) boardHeaderButtons += numberOfCardsButtonHTML;
+    // Inner HTML of header buttons
+    let boardHeaderButtons = document.getElementsByClassName(
+        "board-header-btns mod-left"
+    )[1].innerHTML;
+
+    // Check if button already exists before creating it.
+    if (!buttonInstance) boardHeaderButtons += numberOfCardsButtonHTML;
+    console.log("Created on/off button.")
 })();
